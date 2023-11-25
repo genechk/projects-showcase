8bb8c82beebb2400e235d60a64c10282b9b0e2da
genechk <genek.tomashko@gmail.com>
2023-11-25 22:01:10 +0200

    feat: basic analytics

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -1,2 +1,4 @@
+export { default as AnalyticsScript } from './AnalyticsScript.astro'
+export { default as HeapAnalytics } from './HeapAnalytics.astro'
 export { default as SidebarMenu } from './SidebarMenu.astro'
 export { default as TopMenuNav, type MenuItemData } from './TopMenuNav.astro'
